69a0e8ec6779b9a11dc0e608839f7311999f3bb3
Joshua Owusu <joshbillion1@gmail.com>
2023-04-13 11:30:02 +0000

    Commit auto_update

--- Car-Showroom/src/App.jsx
+++ Car-Showroom/src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 
 import NavBar from './Components/NavBar/NavBar'
 import Home from './Components/Home/Home'
-import  from './Components//'
-import NavBar from './Components/NavBar/NavBar'
-import NavBar from './Components/NavBar/NavBar'
-import NavBar from './Components/NavBar/NavBar'
-import NavBar from './Components/NavBar/NavBar'
-import NavBar from './Components/NavBar/NavBar'
+import Search from './Components/Search/Search'
+import Trending from './Components/Trending/Trending'
+import Sellers from './Components/Sellers/Sellers'
+import Auction from './Components/Auction/Auction'
+import Review from './Components/Review/Review'
+import Footer from './Components/Footer/Footer'
 
 
 // import Auction from './Components/Auction/Auction'
