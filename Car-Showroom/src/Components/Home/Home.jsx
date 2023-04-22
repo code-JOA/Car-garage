0cf287bc7ac284c636169b75768b7272a7f6299a
Joshua Owusu <joshbillion1@gmail.com>
2023-04-22 19:26:17 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Home/Home.jsx
+++ Car-Showroom/src/Components/Home/Home.jsx
@@ -4,22 +4,19 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div className='home'>
-        <div className="secContainer">
-          <div className="homeText">
-            <span className="homeSpan">
-              Meet your new car
-            </span>
-            <h1 className='homeTitle'>
-              Honda Civic Type R
-            </h1>
-            <div className="btn flex">
-              <button className="btn">More Details</button>
-            </div>
+    <div className="home">
+      <div className="secContainer">
+        <div className="homeText">
+          <span className="homeSpan">Meet your new car</span>
+          <h1 className="homeTitle">Honda Civic Type R</h1>
+          <div className="btn flex">
+            <button className="btn">More Details</button>
+            <button className="btn primaryBtn">Test Drive</button>
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
