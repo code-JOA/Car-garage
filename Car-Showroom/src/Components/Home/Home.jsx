58279d0bc9e314840ebaa542aa2d9b63b24a0223
Joshua Owusu <joshbillion1@gmail.com>
2023-04-22 19:11:38 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Home/Home.jsx
+++ Car-Showroom/src/Components/Home/Home.jsx
@@ -5,7 +5,13 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='home'>
-        <div className="secContainer"></div>
+        <div className="secContainer">
+          <div className="homeText">
+            <div className="homeSpan">
+              
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
