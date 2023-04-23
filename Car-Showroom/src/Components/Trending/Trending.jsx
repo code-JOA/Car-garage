2a79ea3f7eafc40917040dcc928b2c47e4996989
Joshua Owusu <joshbillion1@gmail.com>
2023-04-23 17:18:23 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Trending/Trending.jsx
+++ Car-Showroom/src/Components/Trending/Trending.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import './Trending.css'
 
+import { BsArrowLeftShort } from 'react-icons/bs'
+
 const Trending = () => {
   return (
-    <div className='trending'>
+    <div className="trending">
       <div className="secContainer container">
         <div className="secHeading flex">
-          <h3 className="secTitle">
-            Trending Near You
-          </h3>
+          <h3 className="secTitle">Trending Near You</h3>
           <div className="navBtns flex">
-            <BsArrowLeftShort className='icon '/>
+            <BsArrowLeftShort className="icon leftIcon" />
+            <BsArrowRightShort className="icon rightIcon" />
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Trending
