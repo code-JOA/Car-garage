92c85e51d5c38d73316586fc2d0a274ad2b70ad0
Joshua Owusu <joshbillion1@gmail.com>
2023-04-23 11:43:09 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Search/Search.jsx
+++ Car-Showroom/src/Components/Search/Search.jsx
@@ -3,18 +3,19 @@ import './Search.css'
 
 const Search = () => {
   return (
-    <div className='search'>
-        <div className="secContainer Container">
-          <h3 className="title">
-            Which Vehicle Are You Looking For?
-          </h3>
+    <div className="search">
+      <div className="secContainer Container">
+        <h3 className="title">Which Vehicle Are You Looking For?</h3>
 
-          <div className="searchDiv grid">
-            <input type="text" />
-          </div>
+        <div className="searchDiv grid">
+          <input type="text" placeholder="Type" />
+          <input type="text" placeholder="Type" />
+          <input type="text" placeholder="Type" />
+          <input type="text" placeholder="Type" />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Search
