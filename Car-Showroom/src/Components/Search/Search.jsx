573f82ee16cd31c762eac23cff44bd793b13e101
Joshua Owusu <joshbillion1@gmail.com>
2023-04-23 15:27:29 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Search/Search.jsx
+++ Car-Showroom/src/Components/Search/Search.jsx
@@ -13,8 +13,9 @@ const Search = () => {
           <input type="number" placeholder="Year" />
           <input type="text" placeholder="Model" />
           <input type="number" placeholder="Price" />
-          <button className='btn primaryBtn flex'></button>
-          <AiOutlineSearch className='icon'/>
+          <button className='btn primaryBtn flex'>
+            
+          </button>
         </div>
       </div>
     </div>
