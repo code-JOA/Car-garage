a56ca0a2a3c0505934bc9ebab30821fed38fb0c8
Joshua Owusu <joshbillion1@gmail.com>
2023-04-23 20:40:39 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Trending/Trending.jsx
+++ Car-Showroom/src/Components/Trending/Trending.jsx
@@ -5,7 +5,7 @@ import { BsArrowRightShort } from "react-icons/bs"
 import { BsArrowLeftShort } from 'react-icons/bs'
 
 import car6 from '../../Assets/car6.jpg'
-import car5 from '../../Assets/car5.jpg'
+import car4 from '../../Assets/car4.jpg'
 import car7 from "../../Assets/car7.png";
 
 
@@ -43,7 +43,7 @@ const Trending = () => {
           {/* // single car div */}
           <div className="singleCar grid">
             <div className="imgDiv">
-              <img src={car5} alt="carImage" />
+              <img src={car4} alt="carImage" />
               <h5 className="carTitle">Used Audi RS6 Avant</h5>
               <span className="miles">1000 miles only</span>
               <span className="AWD"></span>
