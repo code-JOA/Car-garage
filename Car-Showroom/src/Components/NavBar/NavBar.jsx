93dcae03d65a9e081118f753e07c49182f495750
Joshua Owusu <joshbillion1@gmail.com>
2023-04-23 20:56:29 +0000

    Commit auto_update

--- Car-Showroom/src/Components/NavBar/NavBar.jsx
+++ Car-Showroom/src/Components/NavBar/NavBar.jsx
@@ -24,8 +24,8 @@ const removeNavbar = () => {
 // function for Navbar on scroll
 const [Header, setHeader] = useState('header')
 const addBg = () => {
-    if (window.screenY >= 20{
-        
+    if (window.screenY >= 20){
+        setHeader('header addd')
     }
 }    
     
