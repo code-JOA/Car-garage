0ee1d4ae435fe5b699821ca8e37bc6c495ebfab4
Joshua Owusu <joshbillion1@gmail.com>
2023-04-23 17:22:59 +0000

    Commit auto_update

--- Car-Showroom/src/Components/Trending/Trending.jsx
+++ Car-Showroom/src/Components/Trending/Trending.jsx
@@ -9,7 +9,10 @@ const Trending = () => {
     <div className="trending">
       <div className="secContainer container">
         <div className="secHeading flex">
-          <h3 className="secTitle">Trending Near You</h3>
+          <h3 className="secTitle">
+            Trending Near You
+          </h3>
+          
           <div className="navBtns flex">
             <BsArrowLeftShort className="icon leftIcon" />
             <BsArrowRightShort className="icon rightIcon" />
